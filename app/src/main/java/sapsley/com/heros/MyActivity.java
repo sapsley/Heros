7874b77895417efe23644fe72595a140b58bb400
sapsley <spencer.apsley@gmail.com>
2014-12-08 20:33:02 -0700

    Heros (Source Code)

---
+++ app/src/main/java/sapsley/com/heros/MyActivity.java
@@ -0,0 +1,21 @@
+package sapsley.com.heros;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MyActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Set the layout for fragment_layout.xml
+        setContentView(R.layout.activity_my);
+    }
+
+
+
+}
